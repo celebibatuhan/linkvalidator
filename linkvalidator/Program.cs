045ff363074acb1c9755470190ac6dc5a166958a
Batuhan Çelebi <batuhan.celebi@yemeksepeti.com>
2019-07-04 17:17:47 +0300

    finaş

--- linkvalidator/Program.cs
+++ linkvalidator/Program.cs
@@ -2,36 +2,76 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 
 namespace linkvalidator
 {
     public class Program
-    {
+    {  
 
         public static void Main()
         {
-            Regex rx = new Regex(@"\b(\w*.jpg\w*)|(\w*.png\w*)|(\w*.jpeg\w*)|(\w*.gif\w*)\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            
+            Regex rx = new Regex(@"\b(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
             
             string path = @"C:\Users\batuhan.celebi\source\repos\linkvalidator\linkvalidator\Documents.txt";
             StreamReader text = new StreamReader(path);
 
             
             List<string> links = File.ReadLines(path).ToList();
-            string output;
             for (var i = 0; i < links.Count; i++)
             {
                 MatchCollection matches = rx.Matches(links[i]);
                 
                 foreach (Match match in matches)
                 {
-                    Console.WriteLine("That is what your looking for : {0} ",links[i]);
+                   
+                    Console.WriteLine("That is what you're looking for: " + links[i]);
+                    HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(links[i]);
+                    HttpWebResponse httpRes = null;
+                    try
+                    {
+                        httpRes = (HttpWebResponse)httpReq.GetResponse();
+                        if (httpRes.StatusCode == HttpStatusCode.NotFound)
+                        {
+                            Console.WriteLine("404 Not Found");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Succeed!");
+                        }
+                    }
+                    catch (WebException wec)
+                    {
+                        Console.WriteLine(wec.Status.ToString());
+                        if (wec.Status == WebExceptionStatus.ProtocolError)
+                        {
+                            
+                            Console.WriteLine(string.Format("Status Description : 404 {0} (Invalid Image URL)", ((HttpWebResponse)wec.Response).StatusDescription));
+                            
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                    
+
                 }
             }
-            
-            
-            
-            
+
+
+
+
+
+
+           
+
+
+
+
+
 
         }
 
