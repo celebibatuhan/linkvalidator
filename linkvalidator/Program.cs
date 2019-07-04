7605b94b2ccb627b6da6df94378adab25826125f
Batuhan Çelebi <batuhan.celebi@yemeksepeti.com>
2019-07-04 11:35:36 +0300

    Add project files.

---
+++ linkvalidator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace linkvalidator
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+
+            Regex rx = new Regex(@"\b(\w*.jpg\w*)\b",RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            string path = @"C:\Users\batuhan.celebi\source\repos\linkvalidator\linkvalidator\Documents.txt";
+            StreamReader text = new StreamReader(path);
+            
+            MatchCollection matches = rx.Matches(path);
+
+            Console.WriteLine("{0} matches found in:\n   {1}",matches.Count,text);
+
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                Console.WriteLine("'{0}' repeated at positions {1} and {2}",groups["word"].Value,groups[0].Index, groups[1].Index);
+            }
+
+        }
+
+    }
+}
