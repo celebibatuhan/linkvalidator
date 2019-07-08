8c80924c6bbd5f84ee13ded796d569fe519774b2
Batuhan Çelebi <batuhan.celebi@yemeksepeti.com>
2019-07-08 16:19:43 +0300

    beforegiht

--- linkvalidator/Program.cs
+++ linkvalidator/Program.cs
@@ -5,35 +5,82 @@ using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Data.SqlClient;
-
-
-
+using System.Data;
+using nsexcel=Microsoft.Office.Interop.Excel;
 
 
 namespace linkvalidator 
 {
 
     public class Program
-    {
-        
-
-
+    { 
         public static void Main()
         {
-            //string connetionString = "Data Source=sqltest;Database=YemekSepeti_productcatalog;User id=YEMEKSEPETI\batuhan.celebi";
-            //SqlConnection cnn = new SqlConnection(connetionString);
-            //cnn.Open();
-          
-            //SqlCommand cmd1 = new SqlCommand(
-            //    "SELECT TOP(20) r.DisplayName AS Restaurant, c.DisplayName AS Category, p.DisplayName AS Product, p.ProductId, p.ImagePath " +
-            //    "FROM YemekSepeti_productcatalog.dbo.[TR_ISTANBUL_tr - TR] p(NOLOCK) INNER JOIN YemekSepeti_productcatalog.dbo.[TR_ISTANBUL_tr - TR] c(NOLOCK) ON c.CategoryName = p.PrimaryParentCategory " +
-            //    "AND p.CatalogName = c.CatalogName INNER JOIN YemekSepeti_productcatalog.dbo.[TR_ISTANBUL_tr - TR] r(NOLOCK) ON r.CategoryName = c.PrimaryParentCategory AND r.CatalogName = c.CatalogName " +
-            //    "WHERE p.DefinitionName = 'food' and p.ImagePath != '...'", cnn);
+            string connectionString = "Data Source=sqltest;Initial Catalog=YemekSepeti_productcatalog;" + "Integrated Security=true;";
 
-            //Console.WriteLine(cmd1);
-            //Console.WriteLine();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("State: {0}", connection.State);
 
-            //cnn.Close();
+                string rname, cname, pname, ipath, pid;
+
+                string query = @"SELECT top 1000 r.DisplayName as Restaurant, c.DisplayName AS Category, p.DisplayName AS Product, p.ImagePath, p.ProductId
+                                FROM YemekSepeti_productcatalog.dbo.[TR_ISTANBUL_tr-TR] p(NOLOCK) 
+                                INNER JOIN YemekSepeti_productcatalog.dbo.[TR_ISTANBUL_tr-TR] c(NOLOCK) ON c.CategoryName = p.PrimaryParentCategory
+                                AND p.CatalogName = c.CatalogName 
+                                INNER JOIN YemekSepeti_productcatalog.dbo.[TR_ISTANBUL_tr-TR] r(NOLOCK) ON r.CategoryName = c.PrimaryParentCategory 
+                                AND r.CatalogName = c.CatalogName 
+                                WHERE p.DefinitionName = 'food' and p.ImagePath != '...'";
+
+                
+                string exportPath = @"C:\Users\batuhan.celebi\source\repos\linkvalidator\linkvalidator";
+                string exportCsv = "personexport.csv";
+                StreamWriter csvFile = null;
+
+                try
+                    {
+                        
+                        SqlCommand sqlSelect = new SqlCommand(query, connection);
+
+                        SqlDataReader reader = sqlSelect.ExecuteReader();
+
+                        csvFile = new StreamWriter(@exportPath + exportCsv);
+
+                        csvFile.WriteLine(String.Format("\"{0}\";\"{1}\";\"{2}\";" + "\"{3}\";\"{4}\"", reader.GetName(0), reader.GetName(1), reader.GetName(2),reader.GetName(3), reader.GetName(4)));
+
+                        while (reader.Read())
+                        {
+                            csvFile.WriteLine(String.Format("\"{0}\";\"{1}\";\"{2}\";" + "\"{3}\";\"{4}\"", reader[0], reader[1], reader[2], reader[3], reader[4]));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                    Console.WriteLine("Data export unsuccessful.");
+                    System.Environment.Exit(0);
+                }
+                    finally
+                    {
+                    connection.Close();
+                    csvFile.Close();
+
+                }
+
+                //while (dr.Read())
+                //{
+                //    rname = dr.GetString(0);
+                //    cname = dr.GetString(1);
+                //    pname = dr.GetString(2);
+                //    ipath = dr.GetString(3);
+                //    pid = dr.GetString(4);
+
+                //    Console.WriteLine("Restoran adı {0}, Kategori{1}, Ürün{2}, ImagePath {3}, Ürün ID{4}", rname, cname, pname, ipath, pid);
+                //}
+
+                //dr.Close();
+                //connection.Close();
+
+            }
 
             Regex rx = new Regex(@"\b(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
@@ -76,11 +123,11 @@ namespace linkvalidator
                         }
                     }
 
-                   
+
                 }
             }
 
-            
+
         }
     }
 }
